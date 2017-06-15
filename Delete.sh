0e27097fdcc55a64ccc061c7d203d34537faa8e6
FrogyX <ndieyplus@gmail.com>
2017-06-15 21:46:38 +0800

    Create Delete.sh

--- Delete.sh
+++ Delete.sh
@@ -2,8 +2,8 @@
 #!/bin/bash
 #Script Delete Account SSH & VPN
 #
-read -p "Username Account going to be delete: " Nama
-userdel -r $Nama
+read -p "Username Account going to be delete: " USer
+userdel -r $User
 echo "==============================================="
 echo "DOCTYPE | Explore Network Unlimited"
 echo "==============================================="
