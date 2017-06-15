65dfa96518ce2c4a026a3f0bd1391b7999b6de96
FrogyX <ndieyplus@gmail.com>
2017-06-15 21:48:25 +0800

    Create Active.sh

---
+++ Active.sh
@@ -0,0 +1,45 @@
+#
+#!/bin/bash
+#Script Online User List
+#
+data=( `ps aux | grep -i dropbear | awk '{print $2}'`);
+
+echo "==============================================="
+echo "Active | Checking Online Dropbear"
+echo "-----------------------------------------------------------------------------------"
+
+for PID in "${data[@]}"
+do
+	#echo "check $PID";
+	NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+	USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+	IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+	if [ $NUM -eq 1 ]; then
+		echo "$PID - $USER - $IP";
+	fi
+done
+
+echo "";
+
+data=( `ps aux | grep "\[priv\]" | sort -k 72 | awk '{print $2}'`);
+
+echo "==============================================="
+echo "Active | Checking Online SSH"
+echo "==============================================="
+
+for PID in "${data[@]}"
+do
+	#echo "check $PID";
+	NUM=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | wc -l`;
+	USER=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $10}'`;
+	IP=`cat /var/log/auth.log | grep -i dropbear | grep -i "Password auth succeeded" | grep "dropbear\[$PID\]" | awk '{print $12}'`;
+	if [ $NUM -eq 1 ]; then
+		echo "$PID - $USER - $IP";
+	fi
+done
+
+echo "";
+echo "==============================================="
+echo "DOCTYPE | Explore Network Unlimited"
+echo "==============================================="
+echo ""
