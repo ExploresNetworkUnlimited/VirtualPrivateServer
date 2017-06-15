6d936dfb60147089e833a989dc9a4f9419bd8793
FrogyX <ndieyplus@gmail.com>
2017-06-15 22:33:30 +0800

    Create Debian7x86.sh

--- Debian7x86.sh
+++ Debian7x86.sh
@@ -2,80 +2,54 @@
 #Original Script Copyright www.fornesia.com
 #By DOCT | Explore Network Unlimited
 #
-#####################################
 # Initialization var
-#####################################
 export DEBIAN_FRONTEND=noninteractive
 OS=`uname -m`;
 MYIP=$(wget -qO- ipv4.icanhazip.com);
 MYIP2="s/xxxxxxxxx/$MYIP/g";
 
-#####################################
 # Root USer
-#####################################
 cd
 
-#####################################
 # Disable IPv6
-#####################################
 echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6
 sed -i '$ i\echo 1 > /proc/sys/net/ipv6/conf/all/disable_ipv6' /etc/rc.local
 
-#####################################
 # Install Wget & Curl
-#####################################
 apt-get update; apt-get -y install wget curl;
 
-#####################################
 # Set Time To GMT+8
-#####################################
 ln -fs /usr/share/zoneinfo/Asia/Kuala_Lumpur /etc/localtime
 
-#####################################
 # Set Locale
-#####################################
 sed -i 's/AcceptEnv/#AcceptEnv/g' /etc/ssh/sshd_config
 service ssh restart
 
-#####################################
 # Set Repo
-#####################################
 wget -O /etc/apt/sources.list "https://raw.githubusercontent.com/ForNesiaFreak/FNS_Debian7/fornesia.com/null/sources.list.debian7"
 wget "http://www.dotdeb.org/dotdeb.gpg"
 wget "http://www.webmin.com/jcameron-key.asc"
 cat dotdeb.gpg | apt-key add -;rm dotdeb.gpg
 cat jcameron-key.asc | apt-key add -;rm jcameron-key.asc
 
-#####################################
 # Update
-#####################################
 apt-get update
 
-#####################################
 # Install Nginx
-#####################################
 apt-get -y install nginx
 
-#####################################
 # Install Essential Package
-#####################################
 apt-get -y install bmon iftop htop nmap axel nano iptables traceroute sysv-rc-conf dnsutils bc nethogs openvpn vnstat less screen psmisc apt-file whois ptunnel ngrep mtr git zsh mrtg snmp snmpd snmp-mibs-downloader unzip unrar rsyslog debsums rkhunter
 apt-get -y install build-essential
 
-#####################################
 # Disable Exim
-#####################################
 service exim4 stop
 sysv-rc-conf exim4 off
 
-#####################################
 # Update Files
-#####################################
 apt-file update
 
-#####################################
 # Install Webserver
-#####################################
 cd
 rm /etc/nginx/sites-enabled/default
 rm /etc/nginx/sites-available/default
@@ -85,9 +59,7 @@ echo "<pre>DOCT | Explore Network Unlimited</pre>" > /home/vps/public_html/index
 wget -O /etc/nginx/conf.d/vps.conf "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/vps.conf"
 service nginx restart
 
-#####################################
 # Install OpenVPN
-#####################################
 wget -O /etc/openvpn/openvpn.tar "https://raw.github.com/arieonline/autoscript/master/conf/openvpn-debian.tar"
 cd /etc/openvpn/
 tar xf openvpn.tar
@@ -101,25 +73,19 @@ wget -O /etc/network/if-up.d/iptables "https://raw.githubusercontent.com/nifira1
 chmod +x /etc/network/if-up.d/iptables
 service openvpn restart
 
-#####################################
 # Configure OpenVPN
-#####################################
 cd /etc/openvpn/
 wget -O /etc/openvpn/client.ovpn "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/client-1194.conf"
 sed -i $MYIP2 /etc/openvpn/client.ovpn;
 cp client.ovpn /home/vps/public_html/
 cd
 
-#####################################
 # Configure Port SSH
-#####################################
 sed -i 's/Port 22/Port 22/g' /etc/ssh/sshd_config
 sed -i '/Port 22/a Port 80' /etc/ssh/sshd_config
 service ssh restart
 
-#####################################
 # Install Dropbear
-#####################################
 apt-get -y install dropbear
 sed -i 's/NO_START=1/NO_START=0/g' /etc/default/dropbear
 sed -i 's/DROPBEAR_PORT=22/DROPBEAR_PORT=443/g' /etc/default/dropbear
@@ -131,17 +97,13 @@ service dropbear restart
 
 cd
 
-#####################################
 # Install Squid3
-#####################################
 apt-get -y install squid3
 wget -O /etc/squid3/squid.conf "https://raw.githubusercontent.com/nifira123/debian7_32bit/master/squid3.conf"
 sed -i $MYIP2 /etc/squid3/squid.conf;
 service squid3 restart
 
-#####################################
 # Install Webmin
-#####################################
 cd
 wget -O webmin-current.deb "http://www.webmin.com/download/deb/webmin-current.deb"
 dpkg -i --force-all webmin-current.deb;
@@ -149,9 +111,7 @@ apt-get -y -f install;
 rm webmin-current.deb
 service webmin restart
 
-###########################
 # Command Script
-###########################
 cd /usr/bin
 wget -O Menu "https://github.com/ExploresNetworkUnlimited/VirtualPrivateServer/blob/master/Menu.sh"
 wget -O Trial "https://github.com/ExploresNetworkUnlimited/VirtualPrivateServer/blob/master/Trial.sh"
@@ -175,9 +135,7 @@ chmod +x /usr/bin/Restart
 
 cd
 
-#####################################
 # Finishing
-#####################################
 cd
 chown -R www-data:www-data /home/vps/public_html
 service nginx start
