03c277f637be286c22b304e0e880f094a534a682
FrogyX <ndieyplus@gmail.com>
2017-06-29 17:30:25 +0800

    Update 01.sh

--- 01.sh
+++ 01.sh
@@ -1,12 +1,14 @@
 #
 #!/bin/bash
 #Script cipta akaun percubaan SSH/VPN
+#Doctype | Malaysian Phreaker Knowledge
 #
+
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
 
-User=trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
+User=Trial`</dev/urandom tr -dc X-Z0-9 | head -c4`
 Day="1"
-Pass=`</dev/urandom tr -dc a-f0-9 | head -c9`
+Pass=Pass `</dev/urandom tr -dc A-C0-9 | head -c9`
 
 useradd -e `date -d "$Day days" +"%Y-%m-%d"` -s /bin/false -M $User
 echo -e "$Pass\n$Pass\n"|passwd $User &> /dev/null
@@ -14,14 +16,14 @@ echo ""
 echo "==============================================="
 echo "Trial | Cipta Akaun Percubaan (1 Hari)"
 echo "==============================================="
-echo -e "IP Address: $IP" 
-echo -e "Port OpenSSH: 22,80"
-echo -e "Port Dropbear: 443, 143"
-echo -e "Squid: 80, 8080"
-echo -e "Config OpenVPN: http://$IP:81/client.ovpn"
+echo -e "IP Address: $IP"
+echo -e "Port OpenSSH: 22"
+echo -e "Port Dropbear: 443"
+echo -e "Squid: 8080"
+echo -e "Config OpenVPN: http://$IP/client.ovpn"
 echo -e "Username: $User"
 echo -e "Password: $Pass"
 echo "==============================================="
-echo "DOCTYPE | Explore Network Unlimited"
+echo "Sunan | Explore Network Unlimited"
 echo "==============================================="
 echo ""
