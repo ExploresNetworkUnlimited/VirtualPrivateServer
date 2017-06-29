5311ebb44a3a4e8f2e4ade28909460597461d6cc
FrogyX <ndieyplus@gmail.com>
2017-06-29 17:39:22 +0800

    Create 06.sh

---
+++ 06.sh
@@ -0,0 +1,13 @@
+#
+#!/bin/bash
+#Script membuka akaun SSH/VPN
+#Sunan | Explore Network Unlimited
+#
+
+read -p "Username Account going to be unlock: " User
+passwd -u $User
+
+echo "==============================================="
+echo "Sunan | Explore Network Unlimited"
+echo "==============================================="
+echo ""
