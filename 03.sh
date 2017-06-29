c7d837952d61a8baf338eb4d3c6de104bf7589bf
FrogyX <ndieyplus@gmail.com>
2017-06-29 17:33:33 +0800

    Update 03.sh

--- 03.sh
+++ 03.sh
@@ -1,23 +1,25 @@
 #
 #!/bin/bash
 #Script senarai akaun pelanggan
+#Sunan | Explore Network Unlimited
 #
+
 echo "==============================================="
 echo "USERNAME           EXPIRED DATE           "
 echo "-----------------------------------------------"
 while read expired
 do
-        Acc="$(echo $expired | cut -d: -f1)"
-        Id="$(echo $expired | grep -v nobody | cut -d: -f3)"
-        Exp="$(chage -l $Acc | grep "Account expires" | awk -F": " '{print $2}')"
-        if [[ $Id -ge 1000 ]]; then
+	Acc="$(echo $expired | cut -d: -f1)"
+	Id="$(echo $expired | grep -v nobody | cut -d: -f3)"
+	Exp="$(chage -l $Acc | grep "Account expires" | awk -F": " '{print $2}')"
+	if [[ $Id -ge 1000 ]]; then
         printf "%-17s %2s\n" "$Acc" "$Exp"
-        fi
+	fi
 done < /etc/passwd
 Total="$(awk -F: '$3 >= 1000 && $1 != "nobody" {print $1}' /etc/passwd | wc -l)"
 echo "-----------------------------------------------"
 echo "Total Account: $Total user"
 echo "==============================================="
-echo "DOCTYPE | Explore Network Unlimited"
+echo "Sunan | Explore Network Unlimited"
 echo "==============================================="
 echo ""
