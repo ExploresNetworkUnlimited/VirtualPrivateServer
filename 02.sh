6fb589ed55ec22e6245ec70374454cf8b826005f
FrogyX <ndieyplus@gmail.com>
2017-08-27 04:55:09 +0800

    Update 02.sh

--- 02.sh
+++ 02.sh
@@ -4,26 +4,26 @@
 #Sunan | Explore Network Unlimited
 #
 
-read -p " Username : " User
-read -p " Password : " Pass
-read -p " Expired  (Days ): " Expday
+read -p " Username : " userName
+read -p " Password : " passWord
+read -p " Expired [Days]: " expDate
 
 IP=`dig +short myip.opendns.com @resolver1.opendns.com`
-useradd -e `date -d "$Expday days" +"%Y-%m-%d"` -s /bin/false -M $User
-Exp="$(chage -l $User | grep "Account expires" | awk -F": " '{print $2}')"
-echo -e "$Pass\n$Pass\n"|passwd $User &> /dev/null
+useradd -e `date -d "$expDate days" +"%Y-%m-%d"` -s /bin/false -M $User
+passWord="$(chage -l $userName | grep "Account expires" | awk -F": " '{print $2}')"
+echo -e "$passWord\n$passWord\n"|passwd $userName &> /dev/null
 
 echo "==============================================="
 echo "Register | Buat Akaun Pelanggan"
 echo "==============================================="
 echo -e " IP Address: $IP" 
-echo -e " Port OpenSSH: 22"
-echo -e " Port Dropbear: 443"
-echo -e " Squid: 8080"
+echo -e " Port OpenSSH: 22, 2020"
+echo -e " Port Dropbear: 443, 4343"
+echo -e " Squid: 8080, 3128"
 echo -e " Config OpenVPN: http://$IP/client.ovpn"
-echo -e " Username: $User"
-echo -e " Password: $Pass"
-echo -e " Expired Days: $Exp"
+echo -e " Username: $userName"
+echo -e " Password: $passWord"
+echo -e " Expired Days: $expDate"
 echo "==============================================="
 echo "Sunan | Explore Network Unlimited"
 echo "==============================================="
